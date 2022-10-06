e4c2f634726c5ef6000f4f335311455296dbfa45
MatheusFranzener <matheus_hohmann@estudante.sc.senai.br>
2022-10-06 08:50:50 -0300

    Implementando packages

---
+++ Intellibank/src/Entities/Conta.java
@@ -0,0 +1,35 @@
+package Entities;
+
+public abstract class Conta {
+
+	private int numero;
+	private String agencia;
+	private double saldo;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public abstract String mostrarDados();
+
+}
---
+++ Intellibank/src/Entities/Loteria.java
@@ -0,0 +1,43 @@
+package Entities;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Loteria {
+
+	Random random = new Random();
+	
+	public ArrayList<Integer> numerosS = new ArrayList<Integer>();
+	public ArrayList<Integer> numerosP = new ArrayList<Integer>();
+	
+	public int numAcertos = 0;
+	public double premio = 10000;
+
+	public void sortearNumeros() {
+		for (int i = 0; i < 6; i++) {
+			numerosS.add(random.nextInt(60));
+		}
+	}
+
+	public void compararNumeros() {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				if (numerosP.get(i) == numerosS.get(j)) {
+					numAcertos++;
+				}
+			}
+		}
+
+		if (numAcertos == 3) {
+			premio *= 0.25;
+		} else if (numAcertos == 4) {
+			premio *= 0.50;
+		} else if (numAcertos == 5) {
+			premio *= 0.75;
+		} else if (numAcertos == 6) {
+			premio *= 1;
+		} else {
+			premio = 0;
+		}
+	}
+
+}
